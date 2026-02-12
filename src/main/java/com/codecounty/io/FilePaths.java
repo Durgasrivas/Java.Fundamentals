9899827b847f2055a2b0962c8e71ff33b9b9e2a1
Namani Durga Srivas <srivasnetha.147@gmail.com>
2026-02-12 10:20:35 +0530

    Java Fundamentals

--- src/main/java/com/codecounty/io/FilePaths.java
+++ src/main/java/com/codecounty/io/FilePaths.java
@@ -1,6 +1,6 @@
 package com.codecounty.io;
 
 public class FilePaths {
-    public static final String SAMPLE_FILE_ABSOLUTE_PATH="C:\\Users\\hiii\\Downloads\\Code\\Srivas\\Java.txt";
-    public static final String SAMPLE_FOLDER_ABSOLUTE_PATH="C:\\Users\\hiii\\Downloads\\Code\\Srivas";
+    public static final String SAMPLE_FILE_ABSOLUTE_PATH="C:\\Users\\Namani Srivas\\Downloads\\Code\\Srivas\\Java.txt";
+    public static final String SAMPLE_FOLDER_ABSOLUTE_PATH="\"C:\\Users\\Namani Srivas\\Downloads\\Code\\Srivas";
 }
